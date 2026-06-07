68e5488d2d30d39b1f4baff5476596c56bc0f3cd
jixiuf <jixiuf@qq.com>
2026-06-08 00:58:58 +0800

    leadkey-autoloads.el and leadkey-pkg.el should not commited

--- leadkey-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from leadkey.el  -*- no-byte-compile: t; lexical-binding: t -*-
-(define-package "leadkey" "0.2.0" "Translate leader keys to key sequences" '((emacs "30.1")) :kind vc :commit "a2b9597e04ed80e5a0e1791dc44639f6c4188ecc")
